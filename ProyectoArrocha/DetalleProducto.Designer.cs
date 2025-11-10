c8028909c83937616da5c1c7d05e63e2bdec3a79
Elnazario11 <odagez.1904@gmail.com>
2025-11-10 16:59:08 -0500

    cambio

---
+++ ProyectoArrocha/DetalleProducto.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace ProyectoArrocha
+{
+    partial class DetalleProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DetalleProducto));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbcar = new System.Windows.Forms.PictureBox();
+            this.pbperf = new System.Windows.Forms.PictureBox();
+            this.lbperf = new System.Windows.Forms.Label();
+            this.pbham = new System.Windows.Forms.PictureBox();
+            this.pblogo = new System.Windows.Forms.PictureBox();
+            this.pbdeli = new System.Windows.Forms.PictureBox();
+            this.lbdeli = new System.Windows.Forms.Label();
+            this.pbbusq = new System.Windows.Forms.PictureBox();
+            this.lblogo = new System.Windows.Forms.Label();
+            this.tbbusq = new System.Windows.Forms.TextBox();
+            this.pbImagen = new System.Windows.Forms.PictureBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbDescripcion = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbperf)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbham)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pblogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbdeli)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbbusq)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.pbcar);
+            this.panel1.Controls.Add(this.pbperf);
+            this.panel1.Controls.Add(this.lbperf);
+            this.panel1.Controls.Add(this.pbham);
+            this.panel1.Controls.Add(this.pblogo);
+            this.panel1.Controls.Add(this.pbdeli);
+            this.panel1.Controls.Add(this.lbdeli);
+            this.panel1.Controls.Add(this.pbbusq);
+            this.panel1.Controls.Add(this.lblogo);
+            this.panel1.Controls.Add(this.tbbusq);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1009, 84);
+            this.panel1.TabIndex = 68;
+            // 
+            // pbcar
+            // 
+            this.pbcar.Image = ((System.Drawing.Image)(resources.GetObject("pbcar.Image")));
+            this.pbcar.Location = new System.Drawing.Point(955, 24);
+            this.pbcar.Margin = new System.Windows.Forms.Padding(4);
+            this.pbcar.Name = "pbcar";
+            this.pbcar.Size = new System.Drawing.Size(41, 40);
+            this.pbcar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbcar.TabIndex = 6;
+            this.pbcar.TabStop = false;
+            this.pbcar.Click += new System.EventHandler(this.pbcar_Click);
+            // 
+            // pbperf
+            // 
+            this.pbperf.Image = ((System.Drawing.Image)(resources.GetObject("pbperf.Image")));
+            this.pbperf.Location = new System.Drawing.Point(823, 27);
+            this.pbperf.Name = "pbperf";
+            this.pbperf.Size = new System.Drawing.Size(38, 35);
+            this.pbperf.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbperf.TabIndex = 9;
+            this.pbperf.TabStop = false;
+            // 
+            // lbperf
+            // 
+            this.lbperf.AutoSize = true;
+            this.lbperf.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbperf.ForeColor = System.Drawing.Color.White;
+            this.lbperf.Location = new System.Drawing.Point(868, 36);
+            this.lbperf.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbperf.Name = "lbperf";
+            this.lbperf.Size = new System.Drawing.Size(0, 18);
+            this.lbperf.TabIndex = 2;
+            // 
+            // pbham
+            // 
+            this.pbham.Image = ((System.Drawing.Image)(resources.GetObject("pbham.Image")));
+            this.pbham.Location = new System.Drawing.Point(20, 21);
+            this.pbham.Margin = new System.Windows.Forms.Padding(4);
+            this.pbham.Name = "pbham";
+            this.pbham.Size = new System.Drawing.Size(37, 37);
+            this.pbham.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbham.TabIndex = 8;
+            this.pbham.TabStop = false;
+            // 
+            // pblogo
+            // 
+            this.pblogo.Image = ((System.Drawing.Image)(resources.GetObject("pblogo.Image")));
+            this.pblogo.Location = new System.Drawing.Point(77, 21);
+            this.pblogo.Margin = new System.Windows.Forms.Padding(4);
+            this.pblogo.Name = "pblogo";
+            this.pblogo.Size = new System.Drawing.Size(49, 43);
+            this.pblogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pblogo.TabIndex = 7;
+            this.pblogo.TabStop = false;
+            // 
+            // pbdeli
+            // 
+            this.pbdeli.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pbdeli.Image = ((System.Drawing.Image)(resources.GetObject("pbdeli.Image")));
+            this.pbdeli.InitialImage = null;
+            this.pbdeli.Location = new System.Drawing.Point(606, 27);
+            this.pbdeli.Margin = new System.Windows.Forms.Padding(4);
+            this.pbdeli.Name = "pbdeli";
+            this.pbdeli.Size = new System.Drawing.Size(45, 37);
+            this.pbdeli.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbdeli.TabIndex = 1;
+            this.pbdeli.TabStop = false;
+            // 
+            // lbdeli
+            // 
+            this.lbdeli.AutoSize = true;
+            this.lbdeli.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbdeli.ForeColor = System.Drawing.Color.White;
+            this.lbdeli.Location = new System.Drawing.Point(659, 16);
+            this.lbdeli.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbdeli.Name = "lbdeli";
+            this.lbdeli.Size = new System.Drawing.Size(146, 54);
+            this.lbdeli.TabIndex = 1;
+            this.lbdeli.Text = "Delivery Recetario\r\n(507) 6815-1053\r\n(507) 6645-4943";
+            // 
+            // pbbusq
+            // 
+            this.pbbusq.Image = ((System.Drawing.Image)(resources.GetObject("pbbusq.Image")));
+            this.pbbusq.Location = new System.Drawing.Point(563, 31);
+            this.pbbusq.Margin = new System.Windows.Forms.Padding(4);
+            this.pbbusq.Name = "pbbusq";
+            this.pbbusq.Size = new System.Drawing.Size(31, 32);
+            this.pbbusq.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbbusq.TabIndex = 5;
+            this.pbbusq.TabStop = false;
+            // 
+            // lblogo
+            // 
+            this.lblogo.AutoSize = true;
+            this.lblogo.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblogo.ForeColor = System.Drawing.Color.White;
+            this.lblogo.Location = new System.Drawing.Point(134, 30);
+            this.lblogo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblogo.Name = "lblogo";
+            this.lblogo.Size = new System.Drawing.Size(98, 27);
+            this.lblogo.TabIndex = 3;
+            this.lblogo.Text = "ARROCHA";
+            // 
+            // tbbusq
+            // 
+            this.tbbusq.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.tbbusq.Location = new System.Drawing.Point(261, 33);
+            this.tbbusq.Margin = new System.Windows.Forms.Padding(4);
+            this.tbbusq.Name = "tbbusq";
+            this.tbbusq.Size = new System.Drawing.Size(294, 22);
+            this.tbbusq.TabIndex = 0;
+            this.tbbusq.Text = "¿Qué estás buscando?";
+            // 
+            // pbImagen
+            // 
+            this.pbImagen.Image = ((System.Drawing.Image)(resources.GetObject("pbImagen.Image")));
+            this.pbImagen.Location = new System.Drawing.Point(77, 117);
+            this.pbImagen.Margin = new System.Windows.Forms.Padding(4);
+            this.pbImagen.Name = "pbImagen";
+            this.pbImagen.Size = new System.Drawing.Size(257, 343);
+            this.pbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImagen.TabIndex = 69;
+            this.pbImagen.TabStop = false;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblNombre.Location = new System.Drawing.Point(415, 117);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(510, 25);
+            this.lblNombre.TabIndex = 70;
+            this.lblNombre.Text = "CeraVe Reparadora para el Contorno de Ojos 14 ml";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblPrecio.Location = new System.Drawing.Point(478, 165);
+            this.lblPrecio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(58, 20);
+            this.lblPrecio.TabIndex = 71;
+            this.lblPrecio.Text = "$15.35\r\n";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(606, 398);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(362, 62);
+            this.button1.TabIndex = 72;
+            this.button1.Text = "Añadir al carrito";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(436, 395);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(140, 65);
+            this.groupBox2.TabIndex = 73;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cantidad";
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox2.Location = new System.Drawing.Point(11, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(99, 22);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "1";
+            // 
+            // tbDescripcion
+            // 
+            this.tbDescripcion.Location = new System.Drawing.Point(482, 200);
+            this.tbDescripcion.Multiline = true;
+            this.tbDescripcion.Name = "tbDescripcion";
+            this.tbDescripcion.Size = new System.Drawing.Size(441, 175);
+            this.tbDescripcion.TabIndex = 74;
+            this.tbDescripcion.Text = "La Crema Reparadora para el Contorno de ";
+            this.tbDescripcion.TextChanged += new System.EventHandler(this.tbDescripcion_TextChanged);
+            // 
+            // DetalleProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 497);
+            this.Controls.Add(this.tbDescripcion);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.pbImagen);
+            this.Controls.Add(this.panel1);
+            this.Name = "DetalleProducto";
+            this.Text = "DetalleProducto";
+            this.Load += new System.EventHandler(this.DetalleProducto_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbperf)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbham)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pblogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbdeli)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbbusq)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pbcar;
+        private System.Windows.Forms.PictureBox pbperf;
+        private System.Windows.Forms.Label lbperf;
+        private System.Windows.Forms.PictureBox pbham;
+        private System.Windows.Forms.PictureBox pblogo;
+        private System.Windows.Forms.PictureBox pbdeli;
+        private System.Windows.Forms.Label lbdeli;
+        private System.Windows.Forms.PictureBox pbbusq;
+        private System.Windows.Forms.Label lblogo;
+        private System.Windows.Forms.TextBox tbbusq;
+        private System.Windows.Forms.PictureBox pbImagen;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbDescripcion;
+    }
+}
---
+++ ProyectoArrocha/DetalleProducto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoArrocha
+{
+    public partial class DetalleProducto : Form
+    {
+        private string nombre;
+        private decimal precio;
+        private Image imagen;
+        public DetalleProducto(string nombre, decimal precio, Image imagen)
+        {
+            InitializeComponent();
+            this.nombre = nombre;
+            this.precio = precio;
+            this.imagen = imagen;
+        }
+
+        private void pbcar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DetalleProducto_Load(object sender, EventArgs e)
+        {
+            lblNombre.Text = nombre;
+            lblPrecio.Text = "$" + precio.ToString("0.00");
+            if (imagen != null)
+            {
+                pbImagen.Image = imagen;
+                pbImagen.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        private void tbDescripcion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
