2eb2e39438f154f1efc4aa925df891f79ff90edb
alexanderaguilar1906 <alexanderaguilar1906@gmail.com>
2025-11-10 14:01:29 -0500

    pantalla de carrito

---
+++ ProyectoArrocha/Carrito.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Proyecto_ds4
+{
+    partial class Carrito
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.DataGridView dgvCarrito;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblTotalMonto;
+        private System.Windows.Forms.Button btnPagar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnVolver;
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Carrito));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.dgvCarrito = new System.Windows.Forms.DataGridView();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblTotalMonto = new System.Windows.Forms.Label();
+            this.btnPagar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbcarrito = new System.Windows.Forms.PictureBox();
+            this.pbperf = new System.Windows.Forms.PictureBox();
+            this.lbperf = new System.Windows.Forms.Label();
+            this.pbham = new System.Windows.Forms.PictureBox();
+            this.pblogo = new System.Windows.Forms.PictureBox();
+            this.pbdeli = new System.Windows.Forms.PictureBox();
+            this.lbdeli = new System.Windows.Forms.Label();
+            this.pbbusq = new System.Windows.Forms.PictureBox();
+            this.lblogo = new System.Windows.Forms.Label();
+            this.tbbusq = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarrito)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcarrito)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbperf)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbham)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pblogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbdeli)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbbusq)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.Location = new System.Drawing.Point(34, 85);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(141, 37);
+            this.lblTitulo.TabIndex = 5;
+            this.lblTitulo.Text = "Su carrito";
+            // 
+            // dgvCarrito
+            // 
+            this.dgvCarrito.AllowUserToAddRows = false;
+            this.dgvCarrito.AllowUserToDeleteRows = false;
+            this.dgvCarrito.AllowUserToResizeRows = false;
+            this.dgvCarrito.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCarrito.BackgroundColor = System.Drawing.Color.White;
+            this.dgvCarrito.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCarrito.Location = new System.Drawing.Point(40, 139);
+            this.dgvCarrito.MultiSelect = false;
+            this.dgvCarrito.Name = "dgvCarrito";
+            this.dgvCarrito.RowHeadersVisible = false;
+            this.dgvCarrito.RowHeadersWidth = 51;
+            this.dgvCarrito.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCarrito.Size = new System.Drawing.Size(937, 320);
+            this.dgvCarrito.TabIndex = 1;
+            this.dgvCarrito.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCarrito_CellValueChanged);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTotal.Location = new System.Drawing.Point(640, 480);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(77, 32);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "Total:";
+            // 
+            // lblTotalMonto
+            // 
+            this.lblTotalMonto.AutoSize = true;
+            this.lblTotalMonto.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTotalMonto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblTotalMonto.Location = new System.Drawing.Point(720, 480);
+            this.lblTotalMonto.Name = "lblTotalMonto";
+            this.lblTotalMonto.Size = new System.Drawing.Size(77, 32);
+            this.lblTotalMonto.TabIndex = 3;
+            this.lblTotalMonto.Text = "$0.00";
+            // 
+            // btnPagar
+            // 
+            this.btnPagar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnPagar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPagar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnPagar.ForeColor = System.Drawing.Color.White;
+            this.btnPagar.Location = new System.Drawing.Point(834, 469);
+            this.btnPagar.Name = "btnPagar";
+            this.btnPagar.Size = new System.Drawing.Size(143, 48);
+            this.btnPagar.TabIndex = 2;
+            this.btnPagar.Text = "Pagar";
+            this.btnPagar.UseVisualStyleBackColor = false;
+            this.btnPagar.Click += new System.EventHandler(this.btnPagar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.LightGray;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnEliminar.Location = new System.Drawing.Point(40, 480);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(171, 37);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar producto";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnVolver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVolver.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnVolver.Location = new System.Drawing.Point(229, 480);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(114, 37);
+            this.btnVolver.TabIndex = 0;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = false;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.pbcarrito);
+            this.panel1.Controls.Add(this.pbperf);
+            this.panel1.Controls.Add(this.lbperf);
+            this.panel1.Controls.Add(this.pbham);
+            this.panel1.Controls.Add(this.pblogo);
+            this.panel1.Controls.Add(this.pbdeli);
+            this.panel1.Controls.Add(this.lbdeli);
+            this.panel1.Controls.Add(this.pbbusq);
+            this.panel1.Controls.Add(this.lblogo);
+            this.panel1.Controls.Add(this.tbbusq);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1029, 84);
+            this.panel1.TabIndex = 37;
+            // 
+            // pbcarrito
+            // 
+            this.pbcarrito.Image = ((System.Drawing.Image)(resources.GetObject("pbcarrito.Image")));
+            this.pbcarrito.Location = new System.Drawing.Point(963, 23);
+            this.pbcarrito.Margin = new System.Windows.Forms.Padding(4);
+            this.pbcarrito.Name = "pbcarrito";
+            this.pbcarrito.Size = new System.Drawing.Size(41, 40);
+            this.pbcarrito.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbcarrito.TabIndex = 6;
+            this.pbcarrito.TabStop = false;
+            // 
+            // pbperf
+            // 
+            this.pbperf.Image = ((System.Drawing.Image)(resources.GetObject("pbperf.Image")));
+            this.pbperf.Location = new System.Drawing.Point(823, 27);
+            this.pbperf.Name = "pbperf";
+            this.pbperf.Size = new System.Drawing.Size(38, 35);
+            this.pbperf.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbperf.TabIndex = 9;
+            this.pbperf.TabStop = false;
+            // 
+            // lbperf
+            // 
+            this.lbperf.AutoSize = true;
+            this.lbperf.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbperf.Location = new System.Drawing.Point(868, 36);
+            this.lbperf.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbperf.Name = "lbperf";
+            this.lbperf.Size = new System.Drawing.Size(69, 18);
+            this.lbperf.TabIndex = 2;
+            this.lbperf.Text = "Ingresar";
+            // 
+            // pbham
+            // 
+            this.pbham.Image = ((System.Drawing.Image)(resources.GetObject("pbham.Image")));
+            this.pbham.Location = new System.Drawing.Point(20, 21);
+            this.pbham.Margin = new System.Windows.Forms.Padding(4);
+            this.pbham.Name = "pbham";
+            this.pbham.Size = new System.Drawing.Size(37, 37);
+            this.pbham.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbham.TabIndex = 8;
+            this.pbham.TabStop = false;
+            // 
+            // pblogo
+            // 
+            this.pblogo.Image = ((System.Drawing.Image)(resources.GetObject("pblogo.Image")));
+            this.pblogo.Location = new System.Drawing.Point(77, 21);
+            this.pblogo.Margin = new System.Windows.Forms.Padding(4);
+            this.pblogo.Name = "pblogo";
+            this.pblogo.Size = new System.Drawing.Size(49, 43);
+            this.pblogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pblogo.TabIndex = 7;
+            this.pblogo.TabStop = false;
+            // 
+            // pbdeli
+            // 
+            this.pbdeli.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pbdeli.Image = ((System.Drawing.Image)(resources.GetObject("pbdeli.Image")));
+            this.pbdeli.InitialImage = null;
+            this.pbdeli.Location = new System.Drawing.Point(606, 27);
+            this.pbdeli.Margin = new System.Windows.Forms.Padding(4);
+            this.pbdeli.Name = "pbdeli";
+            this.pbdeli.Size = new System.Drawing.Size(45, 37);
+            this.pbdeli.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbdeli.TabIndex = 1;
+            this.pbdeli.TabStop = false;
+            // 
+            // lbdeli
+            // 
+            this.lbdeli.AutoSize = true;
+            this.lbdeli.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbdeli.Location = new System.Drawing.Point(659, 16);
+            this.lbdeli.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbdeli.Name = "lbdeli";
+            this.lbdeli.Size = new System.Drawing.Size(146, 54);
+            this.lbdeli.TabIndex = 1;
+            this.lbdeli.Text = "Delivery Recetario\r\n(507) 6815-1053\r\n(507) 6645-4943";
+            // 
+            // pbbusq
+            // 
+            this.pbbusq.Image = ((System.Drawing.Image)(resources.GetObject("pbbusq.Image")));
+            this.pbbusq.Location = new System.Drawing.Point(563, 31);
+            this.pbbusq.Margin = new System.Windows.Forms.Padding(4);
+            this.pbbusq.Name = "pbbusq";
+            this.pbbusq.Size = new System.Drawing.Size(31, 32);
+            this.pbbusq.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbbusq.TabIndex = 5;
+            this.pbbusq.TabStop = false;
+            // 
+            // lblogo
+            // 
+            this.lblogo.AutoSize = true;
+            this.lblogo.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblogo.Location = new System.Drawing.Point(134, 30);
+            this.lblogo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblogo.Name = "lblogo";
+            this.lblogo.Size = new System.Drawing.Size(98, 27);
+            this.lblogo.TabIndex = 3;
+            this.lblogo.Text = "ARROCHA";
+            // 
+            // tbbusq
+            // 
+            this.tbbusq.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.tbbusq.Location = new System.Drawing.Point(261, 33);
+            this.tbbusq.Margin = new System.Windows.Forms.Padding(4);
+            this.tbbusq.Name = "tbbusq";
+            this.tbbusq.Size = new System.Drawing.Size(294, 22);
+            this.tbbusq.TabIndex = 0;
+            this.tbbusq.Text = "¿Qué estás buscando?";
+            // 
+            // Carrito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1029, 555);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnPagar);
+            this.Controls.Add(this.lblTotalMonto);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.dgvCarrito);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Carrito";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Carrito de Compras";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarrito)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcarrito)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbperf)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbham)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pblogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbdeli)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbbusq)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pbcarrito;
+        private System.Windows.Forms.PictureBox pbperf;
+        private System.Windows.Forms.Label lbperf;
+        private System.Windows.Forms.PictureBox pbham;
+        private System.Windows.Forms.PictureBox pblogo;
+        private System.Windows.Forms.PictureBox pbdeli;
+        private System.Windows.Forms.Label lbdeli;
+        private System.Windows.Forms.PictureBox pbbusq;
+        private System.Windows.Forms.Label lblogo;
+        private System.Windows.Forms.TextBox tbbusq;
+    }
+}
---
+++ ProyectoArrocha/Carrito.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Proyecto_ds4
+{
+    public partial class Carrito : Form
+    {
+        private int IdUsuario;
+        private int IdCarrito;
+
+        public Carrito(int idUsuario)
+        {
+            InitializeComponent();
+            IdUsuario = idUsuario;
+            CargarCarrito();
+        }
+
+        private void CargarCarrito()
+        {
+            using (MySqlConnection conn = DataBase.GetConnection())
+            {
+                conn.Open();
+
+                // Obtener el carrito activo del usuario
+                string queryCarrito = "SELECT IdCarrito FROM Carritos WHERE IdUsuario = @IdUsuario AND Estado = 'Activo'";
+                MySqlCommand cmdCarrito = new MySqlCommand(queryCarrito, conn);
+                cmdCarrito.Parameters.AddWithValue("@IdUsuario", IdUsuario);
+                object result = cmdCarrito.ExecuteScalar();
+
+                if (result == null)
+                {
+                    MessageBox.Show("No hay un carrito activo.");
+                    return;
+                }
+
+                IdCarrito = Convert.ToInt32(result);
+
+                // Mostrar los productos del carrito
+                string query = @"SELECT 
+                                    dc.IdDetalle,
+                                    p.Nombre AS Producto,
+                                    p.Precio,
+                                    dc.Cantidad,
+                                    dc.Subtotal
+                                FROM DetallesCarrito dc
+                                INNER JOIN Productos p ON dc.IdProducto = p.IdProducto
+                                WHERE dc.IdCarrito = @IdCarrito";
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                adapter.SelectCommand.Parameters.AddWithValue("@IdCarrito", IdCarrito);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dgvCarrito.DataSource = table;
+
+                // Calcular el total
+                decimal total = 0;
+                foreach (DataRow row in table.Rows)
+                    total += Convert.ToDecimal(row["Subtotal"]);
+
+                lblTotalMonto.Text = "$" + total.ToString("F2");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvCarrito.CurrentRow == null) return;
+            int idDetalle = Convert.ToInt32(dgvCarrito.CurrentRow.Cells["IdDetalle"].Value);
+
+            using (MySqlConnection conn = DataBase.GetConnection())
+            {
+                conn.Open();
+                string query = "DELETE FROM DetallesCarrito WHERE IdDetalle = @IdDetalle";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@IdDetalle", idDetalle);
+                cmd.ExecuteNonQuery();
+            }
+
+            CargarCarrito();
+        }
+
+        private void dgvCarrito_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == dgvCarrito.Columns["Cantidad"].Index)
+            {
+                int idDetalle = Convert.ToInt32(dgvCarrito.Rows[e.RowIndex].Cells["IdDetalle"].Value);
+                int cantidad = Convert.ToInt32(dgvCarrito.Rows[e.RowIndex].Cells["Cantidad"].Value);
+
+                using (MySqlConnection conn = DataBase.GetConnection())
+                {
+                    conn.Open();
+                    string query = "UPDATE DetallesCarrito SET Cantidad = @Cantidad WHERE IdDetalle = @IdDetalle";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@Cantidad", cantidad);
+                    cmd.Parameters.AddWithValue("@IdDetalle", idDetalle);
+                    cmd.ExecuteNonQuery();
+                }
+
+                CargarCarrito();
+            }
+        }
+
+        private void btnPagar_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = DataBase.GetConnection())
+            {
+                conn.Open();
+                string query = "UPDATE Carritos SET Estado = 'Pagado' WHERE IdCarrito = @IdCarrito";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@IdCarrito", IdCarrito);
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Compra realizada con éxito.", "Pago exitoso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Perfil perfil = new Perfil("Usuario", "correo@ejemplo.com");
+            perfil.Show();
+            this.Close();
+        }
+    }
+}
